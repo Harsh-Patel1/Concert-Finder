aab42477060d0a022bf254d611899c4e62a0b386
Gabby Schwalm <46929733+gmschwalm@users.noreply.github.com>
2023-12-10 14:09:38 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
 from flask import Flask, render_template, redirect
-app = Flask(__name__)
-
-'''
-from flask import Flask, render_template, redirect, url_for, request
-from flaskext.mysql import MySQL
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(
   __name__,
   template_folder='templates',  # html file folder
-  static_folder='static'  # directory for static files
+  static_folder='static'  # directory for static files (CSS, JavaScript, videos, etc..)
 )
 
-@app.route('/color_redirect', methods=['POST'])
-def color_redirect():
-  colors = request.form["teamcolor"]
-  if colors == "Dark Navy":
-    return redirect(url_for('blue'))
-  else:
-    return redirect(url_for('orange'))
-'''
-app = Flask(
-  __name__,
-  template_folder='templates',  # html file folder
-  static_folder='static'  # directory for static files
-)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/database_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#importing SQL models
+
+
+# Routes and Flask configuration
 @app.route('/')  # default page
 def base_page():
   return render_template('index.html', ) 
@@ -38,10 +29,22 @@ def recordings():
 def search():
   return render_template('search.html', )
 
+@app.route('/search_redirect', )
+def search_redirect():
+  #years = User.query.all()
+  #return 'Hello, these are the users: {}'.format(users)
+  '''
+  year = request.form["year"]
+  if year == "2021":
+    return alert("2020221")
+    #return redirect(url_for('blue'))
+  else:
+    return alert("NOT 2021")
+    #return redirect(url_for('orange'))'''
+
 if __name__ == "__main__":  # Makes sure this is the main process
   app.run(  # Starts the site
     host='0.0.0.0',  # Establishes the host, required for repl to detect the site
     port=81
-    ##port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
   )
 
